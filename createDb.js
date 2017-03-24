3df5b4908b7568d9800d603cab65cd202d874804
Gursewak <gsd10@sfu.ca>
2017-03-23 22:38:28 -0700

    created login with bycrpt function

--- createDb.js
+++ createDb.js
@@ -1,21 +1,32 @@
 var fs = require('fs');
 var models = require('./models');
+const bcrypt = require('bcrypt');
 
 models.sequelize.sync().then(function() {
     console.log('Database models initialized');
     fs.readFile(__dirname + '/testdata/users.json', function(err, data) {
         var user_data = JSON.parse(data);
         var users = user_data.users;
-        for (var i = 0; i < users.length; i++) {
-            var user = users[i];
+
+        users.forEach(function(user) {
+            console.log(user);
+            bcrypt.hash(user.credentials, 10, function(err, hash) {
+              
+            
             models.User.create( {  fullname: user.fullname, 
                             id: user.id,
                             email: user.email, 
                             avatar: user.avatar, 
                             username: user.username, 
-                            credentials: user.credentials, 
+                            credentials: hash, 
                             validated: user.validated });
-        }
+           
+        });
+
+        });            
+           
+           
+        
         fs.readFile(__dirname + '/testdata/leaderboards.json', function(err, data) {
             var leaderboard_data = JSON.parse(data);
             var leaderboards = leaderboard_data.leaderboards;
--- server.js
+++ server.js
@@ -2,14 +2,63 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var https = require('https');
-
+var fs = require('fs');
+var crypto = require('crypto');
+var models = require('./models');
 var app = express();
+const bcrypt = require('bcrypt');
+
+
 app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({   // to support URL-encoded bodies
     extended: true
 }));
 
+var generateToken = function() {
+     var sha = crypto.createHash('sha256');
+    sha.update(Math.random().toString());
+    return sha.digest('hex');
+};
+
+
+app.get('/images/user_photo.png', function(request, response) {
+  response.writeHead(200, {'Content-type' : 'image/gif'});
+    var img = fs.readFileSync('./images/user_photo.png');
+    response.end(img, 'binary');
+});
+
+app.get('/style.css', function(request, response) {
+  response.writeHead(200, {'Content-type' : 'text/css'});
+    var fileContents = fs.readFileSync('./stylesheets/style.css', {encoding: 'utf8'});
+    response.write(fileContents);
+    response.end();
+});
+
+
 app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.post('/', function(request, response) {
+  
+    var user_name = request.body.username;
+    var password = request.body.credential;
+    console.log("post received: %s %s", user_name, password);
+    //bcrypt.compare(password, hash, function(err, res) {
+     models.User.findOne({
+                where: {
+                    username: user_name,
+                    
+                }
+            }).then(function(login) {
+                console.log("Your are successfully login");
+               response.redirect('/home');   
+                
+            });
+
+});
+
+app.get('/home', function(request, response) {
   response.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
@@ -26,6 +75,9 @@ app.post('/recaptcha', function(request, response) {
                 }
         });
 });
+
+
+
 
 app.listen(3000, function () {
 	console.log('Listening on port 3000!');
