b0834a55405346133044399a51cff7d9fe9dac39
Gursewak <gsd10@sfu.ca>
2017-04-08 20:35:44 -0700

    comment removed

--- models/user.js
+++ models/user.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
         User.belongsToMany(models.Achievement, {through: 'UserAchievement'});
       }
     }
->>>>>>> master
+
   });
   return User;
 };
