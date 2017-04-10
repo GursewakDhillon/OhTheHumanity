de1a35440b404176cd4a4c53a63dae5634c94481
Gursewak <gsd10@sfu.ca>
2017-04-09 18:23:53 -0700

    scores in decending order!

--- server.js
+++ server.js
@@ -234,9 +234,9 @@ io.on('connection', function(socket){
 				
 				function predicateBy(prop){
 				   return function(a,b){
-					  if( a[prop] > b[prop]){
+					  if( a[prop] < b[prop]){
 						  return 1;
-					  }else if( a[prop] < b[prop] ){
+					  }else if( a[prop] > b[prop] ){
 						  return -1;
 					  }
 					  return 0;
